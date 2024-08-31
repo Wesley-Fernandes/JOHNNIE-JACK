ea25355b7bb6936cef6634045fc6c9016ac2c7fc
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-08-31 12:14:26 -0300

    feat: loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Loader() {
+  return (
+    <main className='h-[calc(100vh-3.5rem)] flex items-center justify-center'>
+        <Image src="/loader.gif" alt="Loading animation" width={200} height={100} priority />
+    </main>
+  )
+}
