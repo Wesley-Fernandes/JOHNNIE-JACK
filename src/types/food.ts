2d741baa879d1f6cf23de8711f2fcecff997dece
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-08-31 10:41:23 -0300

    feat: food initial types

---
+++ src/types/food.ts
@@ -0,0 +1,45 @@
+//import { Prisma } from "@prisma/client";
+//import { DeliveryPartial } from "./delivery";
+
+import type { Prisma } from "@prisma/client";
+
+export type CategoryTypes = "Bebidas"|"Lanches"|"Pizzas"|"Sobremesas"|"Salgados"|"Doces"|"Cafe"|"Marmita"|"Churrasco"|"Produtos";
+
+export type FoodType = Prisma.FoodGetPayload<{
+    select: {
+        thumbnail: true;
+        name: true;
+        category: true;
+        price: true;
+        description: true;
+        complements: true;
+        enabled: true;
+    }
+}>;
+
+export type ComplementType = Prisma.ComplementGetPayload<{
+    select:{
+        name: true;
+        price: true;
+        thumbnail: true;
+        id?: true;
+    }
+}>
+export interface FoodFullInterface{
+    id: string;
+    thumbnail: string;
+    name: string;
+    category: CategoryTypes;
+    price: number;
+    description: string;
+    complements: ComplementType[];
+};
+
+
+export interface FoodInterface{
+    id: string;
+    thumbnail: string;
+    name: string;
+    category: CategoryTypes;
+    price: number;
+};
